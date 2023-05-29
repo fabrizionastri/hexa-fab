4d7b9f4e0758f034f2f6f09cc2159cc55ad974d4
Fabrizio <fabrizio.nastri@flexup.org>
2023-05-29 11:51:30 +0200

    func: project structure filter

---
+++ filterProjectStructure.ts
@@ -0,0 +1,44 @@
+import * as fs from 'fs'
+
+// Parse the .project_structure_filter file into an array of filter patterns
+const filterPatterns = fs
+  .readFileSync('.project_structure_filter', 'utf-8')
+  .split('\n')
+  .map((line) => line.trim())
+
+console.log('filterPatterns:', filterPatterns)
+
+// checks if the file patch contains any of the filter patterns
+function containsFilter(filePath: string): boolean {
+  return filterPatterns.some((filter) => filePath.includes(filter))
+}
+
+const input = fs.createReadStream('project_structure.txt')
+const output = fs.createWriteStream('project_structure_filtered.txt')
+let keepThisSection = true
+
+input.on('data', (chunk) => {
+  const lines = chunk.toString().split('\n')
+
+  const filteredLines = lines.filter((line) => {
+    // Check if this line starts a file section
+    if (line.startsWith('--- File: ')) {
+      const filePath = line.slice('--- File: '.length).trim()
+      console.log('filePath:', filePath)
+      keepThisSection = containsFilter(filePath)
+      console.log('keepThisSection:', keepThisSection)
+    }
+    // If we're in an section that matches the filter, then add this line to the
+    if (keepThisSection) {
+      return line
+    } else {
+      return false
+    }
+  })
+
+  output.write(filteredLines.join('\n'))
+})
+
+input.on('end', () => {
+  output.end()
+})
