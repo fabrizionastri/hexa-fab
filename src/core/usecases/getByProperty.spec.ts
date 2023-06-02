b22ed032ac8d677f7832d62580b24eb79d386da8
Fabrizio <fabrizio.nastri@flexup.org>
2023-06-02 13:21:19 +0200

    chore: removed useless test

--- src/core/usecases/getByProperty.spec.ts
+++ src/core/usecases/getByProperty.spec.ts
@@ -1,14 +1,13 @@
 import { mockStore } from '../../../mock/db'
-
 import { OrderItemAdapterIM } from '../../adapters/stores/inMemory/orderItem.adapter.im'
-import { JsonServerStore } from '../../adapters/stores/jsonServer/order.adapter.js.js'
+
 // import { Order } from '../../core/entities/order'
 import { OrderItem } from '../entities/orderItem'
 import { getByProperty } from './getByProperty'
 
 const ordersItems = mockStore.orderItems
 
-describe('getByProperty from Mock arrays', () => {
+describe('getByProperty', () => {
   const orderItemStore = OrderItemAdapterIM
   it('return list of 2 childs of Order 1', async () => {
     // const orderInMemoryStore = ordersMock
@@ -24,16 +23,17 @@ describe('getByProperty from Mock arrays', () => {
   })
 })
 
-describe('getByProperty from Json Server', () => {
-  const orderItemStore = JsonServerStore<OrderItem>('orderItems')
-  it('return list of 2 childs of Order 1', async () => {
-    expect(
-      await getByProperty<OrderItem>('orderId', 'order1', orderItemStore)
-    ).toEqual(ordersItems.slice(1, 3))
-  })
-  it('return list of 1 child of Order 2', async () => {
-    expect(
-      await getByProperty<OrderItem>('orderId', 'order2', orderItemStore)
-    ).toEqual(ordersItems.slice(3, 4))
-  })
-})
+// inutil de tester le use case sur les deux adapters
+// describe('getByProperty from Json Server', () => {
+//   const orderItemStore = OrderItemAdapterIM
+//   it('return list of 2 childs of Order 1', async () => {
+//     expect(
+//       await getByProperty<OrderItem>('orderId', 'order1', orderItemStore)
+//     ).toEqual(ordersItems.slice(1, 3))
+//   })
+//   it('return list of 1 child of Order 2', async () => {
+//     expect(
+//       await getByProperty<OrderItem>('orderId', 'order2', orderItemStore)
+//     ).toEqual(ordersItems.slice(3, 4))
+//   })
+// })
